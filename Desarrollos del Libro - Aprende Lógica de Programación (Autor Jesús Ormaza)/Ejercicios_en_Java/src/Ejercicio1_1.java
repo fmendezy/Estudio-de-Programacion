7a420bfbbd7c11ad1c2472afa7550fa5313d4c94
Francisco Mendezy <fmy@outlook.cl>
2023-02-11 09:02:14 -0300

    Creadas las bases del repositorio junto a su estructura en Java para subir los desarrollos de ejercicios.

---
+++ Desarrollos del Libro - Aprende Lógica de Programación (Autor Jesús Ormaza)/Ejercicios_en_Java/src/Ejercicio1_1.java
@@ -0,0 +1,9 @@
+public class Ejercicio1_1 {
+    /*
+     * Ejercicio 1.1 Escribir un programa para mostrar tu nombre
+     * completo con bordes de asteriscos (*) alrededor.
+     */
+    public static void main(String[] args) {
+        System.out.println("*Francisco Mendez*");
+    }
+}
---
+++ Desarrollos del Libro - Aprende Lógica de Programación (Autor Jesús Ormaza)/Ejercicios_en_Java/src/Ejercicio2_2.java
@@ -0,0 +1,20 @@
+public class Ejercicio2_2 {
+/* 
+Ejercicio 2.2 Escribir un programa que realice las siguientes
+operaciones e imprima el resultado en pantalla:
+➢ 10 + 67
+➢ 165 * 4
+➢ 4.76 + 9.82
+➢ 9.89 / 5
+ */
+    public static void main(String[] args) {
+        int a = 10+67;
+        int b = 165*4;
+        double c = 4.76 + 9.82;
+        double d = 9.89 / 5;
+        System.out.println("El resultado de '10 + 67' es: "+a);
+        System.out.println("El resultado de '165 * 4' es: "+b);
+        System.out.println("El resultado de '4.76 + 9.82' es: "+c);
+        System.out.println("El resultado de '9.89 / 5' es: "+d);
+    }
+}
