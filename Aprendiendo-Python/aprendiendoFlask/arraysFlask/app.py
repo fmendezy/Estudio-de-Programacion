b09429e64ee67ee22767582ba6d486f49be1a046
Francisco Mendezy <fmy@outlook.cl>
2023-11-16 14:28:42 -0300

    Agregados apuntes de Flask con arrays y organizadas las carpetas

---
+++ Aprendiendo-Python/aprendiendoFlask/arraysFlask/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Ejemplo manejo de Listas'
+
+
+# 1°er Ruta que recibe un array como parametro en la URL
+# Se puede probar en la url: http://127.0.0.1:5000/array/5,4,3
+@app.route('/array/<int_list>')
+def process_Array(int_list):
+    return f'Array recibido: {int_list}', 200
+
+# 2°da Ruta que recibe un array como datos de un formulario POST
+# Se puede probar en http://127.0.0.1:5000/submit
+@app.route('/submit', methods=['GET', 'POST'])
+def submit_array():
+    if request.method == 'POST':
+        array_data = request.form.get('array').split(',')
+        return f'Array recibido: {array_data}', 200
+    else:
+        return render_template('form.html')
+
+# 3°ra Ruta que envia un array como respuesta JSON
+@app.route('/get_array', methods=['GET'])
+def get_array():
+    example_array = [1, 2, 3, 4, 5]
+    return jsonify(array=example_array)
+
+if __name__ == '__main__':
+    app.run()
