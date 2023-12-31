2852931c8042c297312f45641d7388558d8607b2
Francisco Mendezy <fmy@outlook.cl>
2023-12-31 12:46:24 -0300

    Cargado ejercicio de nulabilidad y comienzo de ejercicios de Kotlin oficial de Google

---
+++ Aprendiendo-Kotlin_Android/Curso_AndroidMaster/app/src/main/java/cl/mendezy/curso_androidmaster/Nulabilidad.kt
@@ -0,0 +1,21 @@
+package cl.mendezy.curso_androidmaster
+
+
+/*
+ * el signo "?" acompañando el tipo de dato significa que se permite que sea nulo.
+ * Ej:
+ * var name:String? = null
+ *
+ * También cuando se ponene dos signos de exclamación: !! al lado de una variable con condición
+ * significa que estamos SEGUROS SEGUROS (sí, dos veces) que el valor no será nulo.
+ *
+ * Ej:
+ * println(name!![3])
+ */
+fun main(){
+    var name:String? = "Francisco"
+
+    println(name?.get(3) ?: "Es nulo")
+}
+
+
