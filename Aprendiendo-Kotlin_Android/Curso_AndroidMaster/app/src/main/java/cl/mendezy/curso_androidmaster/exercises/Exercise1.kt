ea71c70e7ff73a9b98d3000f841c0f4166c91724
Francisco Mendezy <fmy@outlook.cl>
2023-12-31 15:17:46 -0300

    Carga de ejercicio 2

--- Aprendiendo-Kotlin_Android/Curso_AndroidMaster/app/src/main/java/cl/mendezy/curso_androidmaster/exercises/Exercise1.kt
+++ Aprendiendo-Kotlin_Android/Curso_AndroidMaster/app/src/main/java/cl/mendezy/curso_androidmaster/exercises/Exercise1.kt
@@ -21,6 +21,7 @@ fun main() {
 }
 
 fun printNotificationSummary(numberOfMessages: Int) {
+    // Desarollo:
     if(numberOfMessages >= 100){
         println("Your phone is blowing up! You have 99+ notifications.")
     }else{
